ef5dfc756c66c5b3195ffcebc8195a5dfd4550c3
今晚刷题了嘛妹妹 <fairy_vip@2925.com>
2022-08-22 21:30:15 +0800

    refactor: swagger 配置优化

--- src/common/api-swagger-doc/index.js
+++ src/common/api-swagger-doc/index.js
@@ -6,35 +6,8 @@ import dataTransform from '../utils/data-transform';
 /**
  * 配置 swagger-jsdoc
  */
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      description: 'express-demo 接口文档',
-      title: 'Swagger - express-demo',
-      version: '1.0.0'
-    },
-    host: dataTransform.getServiceStartPath(),
-    basePath: '/',
-    produces: ['application/json', 'application/xml'],
-    schemes: ['http', 'https'],
-    securityDefinitions: {
-      JWT: {
-        type: 'apiKey',
-        in: 'header',
-        name: 'Authorization',
-        description: ''
-      }
-    }
-  },
-  route: {
-    url: globalConfig.swagger.apiDocRouter,
-    docs: globalConfig.swagger.apiDocJSONRouter // swagger文件 api
-  },
-  basedir: __dirname, // app absolute path
-  files: ['../../controller/*.js'] // Path to the API handle folder
-};
 
-const options = {
+const swaggerOptions = {
   info: {
     title: 'express-demo 接口文档',
     description: 'express-demo 接口文档',
@@ -79,8 +52,7 @@ const options = {
 
 export default (app) => {
   console.log(swaggerOptions);
-  expressJSDocSwagger(app)(options);
-  // expressSwaggerGenerator(app)(swaggerOptions);
+  expressJSDocSwagger(app)(swaggerOptions);
   logger.info(
     `api swagger docs listening at: ${dataTransform.getApiSwaggerPath()}`
   );
